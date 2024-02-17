6942de152278942e79ee45b011fb58b2f616da59
Harsh Haria <harshharia1234@gmail.com>
2024-02-17 12:03:15 +0530

    Autowiring implementation(byName, byType, constructor) using xml

---
+++ com/springcore/auto/write/Emp.java
@@ -0,0 +1,27 @@
+package com.springcore.auto.write;
+
+public class Emp {
+	private Address address;
+
+	public Address getAddress() {
+		return address;
+	}
+
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+
+	public Emp() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public String toString() {
+		return "Emp [address=" + address + "]";
+	}
+
+	
+	
+	
+}
---
+++ com/springcore/auto/write/Test.java
@@ -0,0 +1,15 @@
+package com.springcore.auto.write;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ApplicationContext context = new ClassPathXmlApplicationContext("com/springcore/auto/write/autowriteConfig.xml");
+		Emp emp1 = context.getBean("emp1", Emp.class);
+		System.out.println(emp1);
+	}
+
+}
