cd665121153511878845a4e4a36ce6e438fc436f
Harsh Haria <harshharia1234@gmail.com>
2024-02-18 19:09:39 +0530

    Scope added for bean lifecycle. Annotations used

--- com/springcore/stereotype/Student.java
+++ com/springcore/stereotype/Student.java
@@ -3,9 +3,11 @@ package com.springcore.stereotype;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component("student")
+@Scope("prototype") //this is for the bean lifecycle.
 public class Student {
 	@Value("Harsh")
 	private String studentName;
--- com/springcore/stereotype/Test.java
+++ com/springcore/stereotype/Test.java
@@ -8,7 +8,11 @@ public class Test {
 	public static void main(String[] args) {
 		ApplicationContext context = new ClassPathXmlApplicationContext("com/springcore/stereotype/stereotypeConfig.xml");
 		Student student = context.getBean("student", Student.class);
-		System.out.println(student);
-		System.out.println(student.getCities());
+//		System.out.println(student);
+//		System.out.println(student.getCities());
+		System.out.println(student.hashCode());
+		
+		Student student2 = context.getBean("student", Student.class);
+		System.out.println(student2.hashCode());
 	}
 }
