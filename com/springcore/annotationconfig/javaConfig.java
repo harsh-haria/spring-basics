c0e8b794897155bdb97f37dd155b14cef46a5770
Harsh Haria <harshharia1234@gmail.com>
2024-03-02 18:42:13 +0530

    added how we can create beans in java class

--- com/springcore/annotationconfig/javaConfig.java
+++ com/springcore/annotationconfig/javaConfig.java
@@ -1,16 +1,14 @@
 package com.springcore.annotationconfig;
 
-//import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan(basePackages = "com.springcore.annotationconfig")
 public class javaConfig {
 	
-//	@Bean
-//	public Student getStudent() {
-//		Student student = new Student();
-//		return student;
-//	}
+	@Bean
+	public Student getStudent() {
+		Student student = new Student();
+		return student;
+	}
 }
