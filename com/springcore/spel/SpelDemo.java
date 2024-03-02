ffbe04b3d62e08c752a4ba89bfe6d569e8a7e876
Harsh Haria <harshharia1234@gmail.com>
2024-03-02 15:57:10 +0530

    Spring expression language examples added

--- com/springcore/spel/SpelDemo.java
+++ com/springcore/spel/SpelDemo.java
@@ -8,8 +8,22 @@ public class SpelDemo {
 
 	@Value("#{2+3}")
 	private int x;
+
 	@Value("#{22*65}")
 	private int y;
+
+	@Value("#{ new java.lang.String('harsh') }")
+	private String name;
+	
+	@Value("#{ T(java.lang.Math).sqrt(256) }")
+	private int finalAnswer;
+	
+	@Value("#{ T(java.lang.Math).E }")
+	private float eVal;
+	
+	@Value("#{ 8-3>2 }")
+	private boolean expression;
+	
 	public int getX() {
 		return x;
 	}
@@ -22,9 +36,35 @@ public class SpelDemo {
 	public void setY(int y) {
 		this.y = y;
 	}
-	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getFinalAnswer() {
+		return finalAnswer;
+	}
+	public void setFinalAnswer(int finalAnswer) {
+		this.finalAnswer = finalAnswer;
+	}
+	public float geteVal() {
+		return eVal;
+	}
+	public void seteVal(float eVal) {
+		this.eVal = eVal;
+	}
+	public boolean isExpression() {
+		return expression;
+	}
+	public void setExpression(boolean expression) {
+		this.expression = expression;
+	}
 	@Override
 	public String toString() {
-		return "SpelDemo [x=" + x + ", y=" + y + "]";
+		return "SpelDemo [x=" + x + ", y=" + y + ", name=" + name + ", finalAnswer=" + finalAnswer + ", eVal=" + eVal
+				+ ", expression=" + expression + "]";
 	}
+	
+	
 }
